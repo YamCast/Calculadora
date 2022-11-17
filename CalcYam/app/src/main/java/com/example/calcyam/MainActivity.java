15465eadb466b3876db611189fd0cf27e7b7d893
YamCast <100479274+YamCast@users.noreply.github.com>
2022-11-17 14:03:01 -0500

    Add files via upload

--- CalcYam/app/src/main/java/com/example/calcyam/MainActivity.java
+++ CalcYam/app/src/main/java/com/example/calcyam/MainActivity.java
@@ -2,13 +2,98 @@ package com.example.calcyam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvdisplay = (TextView) findViewById(R.id.tv_dis);
+    double n1, n2, res;
+    String Operador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void btncero(View View) {
+        tvdisplay= (TextView) findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"0");
+    }
+
+    public void btnuno(View View) {
+        tvdisplay=(TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"1");
+    }
+
+    public void btndos(View View) {
+        tvdisplay= (TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"2");
+    }
+    public void btntres(View View) {
+        tvdisplay= (TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"3");
+    }
+    public void btncuatr(View View) {
+        tvdisplay= (TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"4");
+    }
+
+    public void btncinc(View View) {
+        tvdisplay= (TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"5");
+    }
+    public void btnseis(View View) {
+        tvdisplay= (TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"6");
+    }
+    public void btnsie(View View) {
+        tvdisplay= (TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"7");
+    }
+
+    public void btnoct(View View) {
+        tvdisplay= (TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"8");
+    }
+
+    public void btnnuev(View View) {
+        tvdisplay= (TextView)findViewById(R.id.tv_dis);
+        tvdisplay.setText(tvdisplay.getText()+"9");
+    }
+
+
+    //capturando los datos
+    public void onClickOperacionCapt1(View View){
+        tvdisplay=(TextView) findViewById(R.id.tv_dis);
+        n1=Double.parseDouble(tvdisplay.getText().toString());
+        tvdisplay.setText("");
+    }
+
+
+    //seguimos operaciones
+    public void sumar(View View){
+        Operador="+";
+        onClickOperacionCapt1(View);
+    }
+    public void rest(View View){
+        Operador="-";
+        onClickOperacionCapt1(View);
+    }
+    public void mult(View View){
+        Operador="*";
+        onClickOperacionCapt1(View);
+    }
+    public void div(View View){
+        Operador="/";
+        onClickOperacionCapt1(View);
+    }
+
+
 }
