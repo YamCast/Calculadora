c40ce4ed7d910b342e908ea83fb1e156225d05e5
YamCast <100479274+YamCast@users.noreply.github.com>
2022-11-22 17:42:27 -0500

    Add files via upload

--- CalcYam/app/src/main/java/com/example/calcyam/MainActivity.java
+++ CalcYam/app/src/main/java/com/example/calcyam/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //seguimos operaciones
+    //mostrar simbolos de las operaciones
     public void sumar(View View){
         Operador="+";
         onClickOperacionCapt1(View);
@@ -94,6 +94,96 @@ public class MainActivity extends AppCompatActivity {
         Operador="/";
         onClickOperacionCapt1(View);
     }
+    //es otra operacion
+    public void Expn(View View){
+        Operador="exp";
+        tvdisplay=(TextView) findViewById(R.id.tv_dis);
+        try{
 
+            n1= Double.parseDouble(tvdisplay.getText().toString());
+            tvdisplay.setText("");
+
+        }catch (NumberFormatException nfe) {}
+
+    }
+
+
+    //boton de resultado
+    //operaciones a seguir
+    public void btnigual(View View){
+        tvdisplay=(TextView)findViewById(R.id.tv_dis);
+        n2=Double.parseDouble(tvdisplay.getText().toString());
+        //condicional para rta
+        if(Operador.equals("+")){
+            res=n1+n2;
+        }else if(Operador.equals("-")){
+            res=n1-n2;
+        }else if(Operador.equals("*")){
+            res=n1*n2;
+        }else if(Operador.equals("/")){
+            res=n1/n2;
+        }else if(Operador.equals("exp")){
+            tvdisplay=(TextView)findViewById(R.id.tv_dis);
+            n2=Double.parseDouble(tvdisplay.getText().toString());
+            tvdisplay.setText(" ");
+            res=Math.pov(n1,n2);
+        }
+
+        tvdisplay.setText(" "+res);
+    }
+
+
+    public void Rnd(View View){
+        for(int x=0; x <= 100; x++){
+            int na=(int)Math.floor(Math.random() + (100-(1+1)+(1)));
+            tvdisplay.setText(String.valueOf(na));
+        }
+    }
+
+    public void Raiz(View View){
+        tvdisplay=(TextView)findViewById(R.id.tv_dis);
+        try{
+            n1=Double.parseDouble(tvdisplay.getText().toString());
+            res=Math.sqrt(n1);
+            tvdisplay.setText(String.valueOf(res));
+
+        }catch (NumberFormatException nfe){}
+    }
+
+    public void ExpCuadrado(View View){
+        tvdisplay=(TextView)findViewById(R.id.tv_dis);
+        try{
+
+            n1=Double.parseDouble(tvdisplay.getText().toString());
+            res=Math.pov(n1,2);
+            tvdisplay.setText(String.valueOf(res));
+        }catch (NumberFormatException nfe){}
+    }
+
+//oferación SEN
+    public void Sen(View View){
+        tvdisplay=(TextView)findViewById(R.id.tv_dis);
+        try{
+
+            n1=Double.parseDouble(tvdisplay.getText().toString());
+            double rd=Math.toRadians(n1);
+            res=Math.sin(rd);
+            tvdisplay.setText(String.valueOf(res));
+        }catch (NumberFormatException nfe){}
+
+    }
+
+    //Operacion TAN
+    public void tan(View View){
+        tvdisplay=(TextView)findViewById(R.id.tv_dis);
+        try{
+
+            n1=Double.parseDouble(tvdisplay.getText().toString());
+            double rd=Math.toRadians(n1);
+            res=Math.tan(rd);
+            tvdisplay.setText(String.valueOf(res));
+        }catch (NumberFormatException nfe){}
+
+    }
 
 }
